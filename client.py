5ec34d7e760e68920eb525d2d875b136827909aa
Herbert Ink <81310380+HerbertInk@users.noreply.github.com>
2023-11-17 01:31:44 +0300

    Update client.py
    comments ---caption thing

--- client.py
+++ client.py
@@ -4,7 +4,12 @@ import threading
 def connect_to_server():
     print("\n MNBV Talk \n")
     server_option = input("1. mnbv-server-1  127.0.0.1:63900\n2. Alt IP:Port\n use port 63903: ")
-
+    """
+        The mnbv-servers are for testing on localhost
+        use server ip address for an alternative configuration.
+        Clients on the network initiate connection to a server via 
+        the server's ip address and a dedicated port.
+    """
     try:
         if server_option == '1':
             server_host = '127.0.0.1'
@@ -35,8 +40,16 @@ client_socket.send(username.encode())
 
 def receive_messages():
     while True:
+        """
+            this 'while' loop enables clients to 
+            receive messages on a network
+        """
         try:
             message = client_socket.recv(1024).decode()
+            """
+                1024 maximum amount of data (in bytes)
+                transmitted on a network
+            """
             if not message: # ~!
                 print("server-conn-terminated")
                 client_socket.close()
@@ -48,6 +61,10 @@ def receive_messages():
             print("server-conn-reset")
             client_socket.close()
             exit(1)
+            """
+                on chat exit, the client socket is closed
+                Ctrl+C exits a user from chat
+            """
 
 receive_thread = threading.Thread(target=receive_messages)
 receive_thread.start()
@@ -56,6 +73,10 @@ try:
     while True:
         message = input()
         client_socket.send(message.encode())
+        """
+            this 'while' loop enables clients to 
+            send a message on a network
+        """
 
 except KeyboardInterrupt:
     print("you-exited-chat")
@@ -63,4 +84,9 @@ except KeyboardInterrupt:
 finally: # will-exit
     client_socket.send(b'exit-at-will') 
     client_socket.close()
-    receive_thread.join()
+    receive_thread.join
+    """
+        a message of a connection drop
+        is broadcast to clients on a network
+        when a client exits wilingly by pressing 'Ctrl+C'
+    """
